a3954d9dc558cafb9338d9553224f3cdc9e5ce96
parkmh8484 <35356058+parkmh8484@users.noreply.github.com>
2018-05-18 15:43:56 +0900

    Create MaxCoutners.java
    Time complexity O(n*m) -> O(n+m)

---
+++ MaxCoutners.java
@@ -0,0 +1,23 @@
+// you can also use imports, for example:
+import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int[] solution(int N, int[] A) {
+        // write your code in Java SE 8
+        
+        int[] arr= new int[N];
+        for(int i =0; i<A.length; i++){
+            if(A[i] == N+1){
+                Arrays.sort(arr);
+                Arrays.fill(arr, arr[N-1]);
+            }else{
+                arr[A[i]-1]++;
+            }
+        }
+    return arr;    
+        
+    }
+}
